59a76a57666d9d655cd5f2a2242d70e94b65be8e
zhangzhihua <39785279+zhangsan7824@users.noreply.github.com>
2019-04-21 19:46:57 +0800

    单链表

--- test.scala
+++ test.scala
@@ -8,37 +8,98 @@ object test {
     val node3 = new HerNode(3,"c","03")
 
     val sing = new SingleLinkedList
-  /*  sing.add(node4)
+    sing.add(node4)
     sing.add(node1)
     sing.add(node2)
-    sing.add(node3)*/
-   sing.add2(node1)
+    sing.add(node3)
+   /* sing.add2(node1)
     sing.add2(node2)
     sing.add2(node4)
-    sing.add2(node3)
+    sing.add2(node3)*/
 
+    sing.list()
+    val node5 = new HerNode(3,"无用","智多星")
+    sing.update(node5)
+    println("=============================================")
     sing.list
+    println("===================================")
+    println(sing.del(1))
+    println(sing.del(3))
+    println(sing.del(4))
+    sing.list()
   }
 }
 
-//定义单向列表，链表的无序插入
+//定义单向列表
 class SingleLinkedList {
   //初始化头结点，头结点一般不动
    val head = new HerNode(0,"","")
-  //编写添加的方法
-  def add(herNode: HerNode):Unit={
+  //删除节点
+  def  del(no:Int):Unit={
     var temp = head
+    var flag = false
     breakable {
       while (true) {
         if (temp.next == null) {
           break()
         }
+        if (temp.no == no) {
+          flag = true
+          break()
+        }
         temp = temp.next
       }
     }
-     temp.next = herNode
+    if (flag){
+      temp.next = temp.next.next
+    }else{
+      printf("节点不存在")
+    }
   }
-  //链表的有序插入
+//更新节点的值
+  def update(newHeroNode:HerNode):Unit= {
+    //先找到节点
+    if (head.next == null) {
+      println("链表为kong")
+      return
+    }
+    var temp = head.next
+    var flag = false
+    breakable {
+      while (true) {
+        if (temp == null) {
+          break()
+        }
+        if (temp.no == newHeroNode.no) {
+          flag = true
+          break()
+        }
+        temp = temp.next
+      }
+      }
+    if (flag) {
+      temp.name = newHeroNode.name
+      temp.nickname = newHeroNode.nickname
+    } else {
+      printf("没有找到节点")
+    }
+
+  }
+    //编写添加的方法
+
+    def add(herNode: HerNode): Unit = {
+      var temp = head
+      breakable {
+        while (true) {
+          if (temp.next == null) {
+            break()
+          }
+          temp = temp.next
+        }
+      }
+      temp.next = herNode
+    }
+
   def add2(herNode: HerNode):Unit= {
     var temp = head
     var flag = false //判断编号是否存在，默认不存在
@@ -47,7 +108,7 @@ class SingleLinkedList {
         if (temp.next == null) { //说明temp已经到了链表最后
           break()
         }
-        if (temp.next.no < herNode.no) { //位置找到,当前节点加入到temp后
+        if (temp.next.no > herNode.no) { //位置找到,当前节点加入到temp后
           break()
 
         } else if (temp.next.no == herNode.no) {
@@ -87,7 +148,7 @@ class SingleLinkedList {
 
 class HerNode(hno: Int, hname: String, hnickname: String) {
   val no: Int = hno
-  val name: String = hname
-  val nickname: String = hnickname
+  var name: String = hname
+  var nickname: String = hnickname
   var next: HerNode = null
 }
