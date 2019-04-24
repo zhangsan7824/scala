39bf968ac777226349559928e96dba689e58fea0
zhangzhihua <39785279+zhangsan7824@users.noreply.github.com>
2019-04-25 03:48:19 +0800

    递归

--- diguidemo.scala
+++ diguidemo.scala
@@ -1,16 +1,15 @@
-package cn.scala
 
-object diguidemo {
-  def main(args: Array[String]){
-    test(4)
+package cn.basis
 
+object test04 {
+  def main(args: Array[String]): Unit = {
+
+        ddddd(4)
   }
-  def test(n:Int): Unit={
-    if (n>2){
-      test(n-1)
+  def ddddd(n:Int){
+    if (n>2) {
+      ddddd( n - 1)
     }
-      println("n=" + n)
-
+    println("n = "+ n)
   }
-
 }
